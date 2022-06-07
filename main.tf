b72f59d9dde6b57b61dc35b4284876002e79929a
ramasaa3 <106380315+ramasaa3@users.noreply.github.com>
2022-06-07 09:31:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,15 +2,11 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
 
-  name = "single-instance"
+  name = "single-instance-2"
 
   ami                    = "ami-032068be080ae7f37"
   instance_type          = "t2.micro"
-  key_name               = "user1"
-  monitoring             = true
 
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
+
+
 }
