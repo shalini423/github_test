a17910fd5cc55fc54012f3c39489bc5c537238d6
ramasaa3 <106380315+ramasaa3@users.noreply.github.com>
2022-06-07 09:25:32 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,6 @@ module "ec2_instance" {
   instance_type          = "t2.micro"
   key_name               = "user1"
   monitoring             = true
-  security_groups        = ["sg-086bc44b57bc8843f"]
-  subnet_id              = "subnet-0e8ae3ab11f6cc47b"
 
   tags = {
     Terraform   = "true"
