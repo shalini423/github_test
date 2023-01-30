6db6974eecf66a79733a42cf703748e4ed98dd2f
shalini423 <52390640+shalini423@users.noreply.github.com>
2023-01-31 01:20:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
-resource "azurerm_resource_group" "demoenv0" {
-  name  = "demoenv0resourcegroup"
-  location = "eastus"
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
 }
