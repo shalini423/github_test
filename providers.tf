2e49d21638aa4fe0d137586fa375b0e22bbabce4
shalini423 <52390640+shalini423@users.noreply.github.com>
2023-01-31 01:16:04 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,7 +1,18 @@
 terraform {
+  required_version = ">=0.12"
+
   required_providers {
-    azure = {
-      version = "~> 3.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
     }
   }
+}
+
+provider "azurerm" {
+  features {}
 }
