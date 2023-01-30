892f0c7e11accbb64d30b4c2c43e58ea5f793d35
shalini423 <52390640+shalini423@users.noreply.github.com>
2023-01-31 00:41:57 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,15 +1,6 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
+    Azure = {
+      region = "eastus"  
     }
-  }
-}
-
-
-provider "aws" {
-  region = "eu-central-1"
-  access_key = "AKIAZFSSE76IGZOZWON2"
-  secret_key = "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"
-}
+  
