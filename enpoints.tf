d0d71cb1ff31a110088d5b09ab4840a733c65a27
shalini423 <52390640+shalini423@users.noreply.github.com>
2023-01-31 01:27:15 +0530

    Delete enpoints.tf

--- enpoints.tf
+++
@@ -1,7 +0,0 @@
-module "endpoints" {
-  source = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
-
-  vpc_id             = "vpc-0bf626fdf5982b9e3"
-  security_group_ids = ["sg-086bc44b57bc8843f"]
-  
-}
